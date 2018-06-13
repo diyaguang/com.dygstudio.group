79646f26ea57d43f582381e414f1ce9842698618
diyaguang <diyaguang@163.com>
2018-06-13 08:59:10 +0800

    add db-model project

--- spring-boot-hello/src/main/java/com/dygstudio/group/springboothello/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/dygstudio/group/springboothello/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.dygstudio.group.springboothello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,21 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
 
+    @Autowired
+    //Using this code can remove an error message."Could not autowire. There is more than one bean of'CounterService'of"
+    @Qualifier("counterService")
+    private CounterService counterService;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String index(){
         ServiceInstance instance = client.getLocalServiceInstance();
         logger.info("/hello, host:"+instance.getHost()+", service_id:"+instance.getServiceId());
+        /*
+        * Here is the custom of a statistical information,
+        * through the org.springframework.boot.actuate.metrics.CounterService interface,
+        * custom statistical indicators, and then through the /metrics to see the relevant index information.
+        */
+        counterService.increment("didispace.hello.count");
         return "Hello World";
     }
 }
