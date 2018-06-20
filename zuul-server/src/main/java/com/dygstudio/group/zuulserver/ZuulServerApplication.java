cb2c0866f26d0b0a2736caeb64f18bb69904b61c
diyaguang <diyaguang@163.com>
2018-06-20 14:15:48 +0800

    modify properties

--- zuul-server/src/main/java/com/dygstudio/group/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/dygstudio/group/zuulserver/ZuulServerApplication.java
@@ -1,8 +1,11 @@
 package com.dygstudio.group.zuulserver;
 
+import com.dygstudio.group.zuulserver.config.AccessFilter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +14,10 @@ public class ZuulServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
+    }
 }
+
